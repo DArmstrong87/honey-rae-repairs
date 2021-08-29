2aa5372027f9fe1e4344bbfb8b115b18a9296863
DArmstrong87 <lenzflare@gmail.com>
2021-08-28 23:25:24 -0500

    added delete button to delete service tickets and reloads the page

--- src/components/serviceTickets/TicketList.js
+++ src/components/serviceTickets/TicketList.js
@@ -3,15 +3,21 @@ import { Link, useHistory } from "react-router-dom"
 import "./Tickets.css"
 
 export const TicketList = () => {
-    const [tickets, updateTicket] = useState([])
+    const [tickets, setTicket] = useState([])
     const history = useHistory()
+    const deleteTicket = (id) => {
+        fetch(`http://localhost:8088/serviceTickets/${id}`, {
+            method: "DELETE"
+        })
+            .then(history.go())
+    }
 
     useEffect(
         () => {
             fetch("http://localhost:8088/serviceTickets?_expand=employee&_expand=customer")
                 .then(res => res.json())
                 .then((data) => {
-                    updateTicket(data)
+                    setTicket(data)
                 })
         },
         []
@@ -30,6 +36,9 @@ export const TicketList = () => {
                     (ticket) => {
                         return <p key={`ticket--${ticket.id}`} className={ticket.emergency ? `emergency` : `ticket`}>
                             {ticket.emergency ? "🚑" : ""} <Link to={`/tickets/${ticket.id}`}>{ticket.description}</Link> submitted by {ticket.customer.name} and worked on by {ticket.employee.name}
+                            <button key={`delete--${ticket.id}`} className='deleteButton' onClick={() => {
+                                deleteTicket(ticket.id)
+                            }}>Delete</button>
                         </p>
                     }
                 )
